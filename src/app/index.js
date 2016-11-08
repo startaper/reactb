fcdc09a29864d073e3bdbb9af1c357243e4bc6b8
Alexander Nikolaev <ni-ko-la@mail.ru>
2016-11-08 07:46:32 +0300

    use props

--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,12 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
     render() {
+
+        var user = {
+          name: "Anna",
+          hobbies: ["Sports", "Sex", "Porno"]
+        };
+
         return (
             <div className="container">
                 <div className="row">
@@ -14,7 +20,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Max"} age={27} user={user}>
+                            <p>This is children paragraph</p>
+                        </Home>
                     </div>
                 </div>
             </div>
